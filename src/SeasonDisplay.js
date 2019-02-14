16a9f0bd3d97ed298970fae47be8c7c7ce5715d7
tiffanyadu <tadofdesign@gmail.com>
2019-02-13 21:00:22 -0600

    Add sun and snowflake icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,9 +11,12 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   const text = season === 'winter' ? "Burr it's cold outside" : "Let's hit the beach!";
+  const icon = season === 'winter' ? "snowflake" : "sun";
   return (
     <div>
+      <i className={`${icon} icon`}></i>
       <h1>{text}</h1>
+      <i className={`${icon} icon`}></i>
     </div>
   );
 };
