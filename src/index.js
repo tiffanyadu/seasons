10410c9723763ceaa1f22f983dc89b077b89471f
tiffanyadu <tadofdesign@gmail.com>
2019-02-13 20:07:48 -0600

    Refactor geolocation code and place inside componentDidMount method

--- src/index.js
+++ src/index.js
@@ -8,13 +8,11 @@ class App extends React.Component {
       lat: null,
       errorMessage: ''
     };
+  }
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
   render() {
